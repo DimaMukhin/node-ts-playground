0c9aced78af0e287a8aefe04fdd4a4ae3c5e29d7
DimaMukhin <dima9348@gmail.com>
2018-08-11 17:01:54 -0700

    tested koa-redis-cache types for definitelytyped

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,35 @@
 import * as Koa from 'koa';
 import * as Router from 'koa-router';
+import * as cache from 'koa-redis-cache';
 
 const app = new Koa();
 const router = new Router();
+
+const routeOptions: Array<cache.RouteOptions> = [
+    {
+      route: '/api/test',
+      expire: 60
+    },
+    {
+      route: '/api/users'
+    }
+  ];
+  
+const redisOptions: cache.RedisOptions = {
+    port: 6379,
+    host: 'localhost'
+};
+
+const options: cache.CacheOptions = {
+    prefix: (ctx: Koa.Context) => 'koa-redis-cache:',
+    expire: 30 * 30,
+    passParam: 'skip',
+    routes: routeOptions,
+    onerror: (error: Error) => console.log(error),
+    redis: redisOptions
+};
+
+app.use(cache(options));
 
 router.get('/*', async (ctx) => {
     ctx.body = 'Hello World!';
