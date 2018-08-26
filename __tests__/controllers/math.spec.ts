60476c0050330b60b225e97b6379e11aa492dd24
DimaMukhin <dima9348@gmail.com>
2018-08-26 12:09:35 -0700

    added /users integration test

--- __tests__/controllers/math.spec.ts
+++ __tests__/controllers/math.spec.ts
@@ -1,8 +1,31 @@
-import { addTwoNumbers } from '../../src/controllers/math';
+import * as Koa from 'koa';
+import * as Router from 'koa-router';
+import axios from 'axios';
+
+let server;
+
+beforeAll(async () => {
+    const app = new Koa();
+    const router = new Router();
+
+    // api routes
+    require('../../src/routes')(router);
+    app.use(router.routes());
+    app.use(router.allowedMethods());
+
+    server = app.listen(3000);
+
+    console.log('Server running on port 3000');
+});
+
+afterAll(async () => {
+    server.close();
+});
 
 describe('controllers/math.ts', () => {
-    it('should add two numbers', () => {
-        debugger;
-        expect(addTwoNumbers(10, 20)).toBe(30);
+    it('should add two numbers', async () => {
+        let res = await axios.get('http://localhost:3000/api/users');
+        expect(res.status).toBe(200);
+        expect(res.data).toBe(30);
     });
 });
