2f2e89b308ee4779a6f385b36b1bbc898e76bf51
DimaMukhin <dima9348@gmail.com>
2018-08-26 12:09:20 -0700

    implemented jest debugging

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,9 @@
 module.exports = {
     transform: {
-        "^.+\\.tsx?$": "ts-jest",
+        "^.+\\.ts$": "ts-jest"
     },
-    testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$",
+    testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.(ts|js)$",
     testPathIgnorePatterns: ["/dist/", "/node_modules/"],
     moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+    testEnvironment: "node",
 };
