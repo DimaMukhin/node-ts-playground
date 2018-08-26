e0e02a7328d95551fa30ff539ae07741d06744b3
DimaMukhin <dima9348@gmail.com>
2018-08-26 11:51:19 -0700

    implemented math controller

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,9 +1,10 @@
 import * as Router from 'koa-router';
+import { addTwoNumbers } from '../controllers/math';
 
 const router = new Router();
 
 router.get('/', (ctx: Router.IRouterContext) => {
-    ctx.body = 'ok';
+    ctx.body = addTwoNumbers(10, 20);
 });
 
 module.exports = router.routes();
