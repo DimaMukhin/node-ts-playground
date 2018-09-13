ef0a3522555c9363920def0b519dfa0041506c4c
DimaMukhin <dima9348@gmail.com>
2018-09-12 20:39:54 -0700

    added apollo-server-koa

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,43 @@
 import * as Koa from 'koa';
 import * as Router from 'koa-router';
+import { ApolloServer, gql } from 'apollo-server-koa';
 
 const app = new Koa();
 const router = new Router();
+
+const schema = gql`
+  type Query {
+    me: User
+    user: User
+  }
+
+  type User {
+    username: String!
+  }
+`;
+
+const resolvers = [{
+    Query: {
+        me: () => {
+            return {
+                username: 'Robin Wieruch',
+            };
+        },
+        user: () => {
+            return {
+                username: 'Dave Davids',
+            };
+        },
+    },
+}];
+
+// @ts-ignore
+const server = new ApolloServer({
+    typeDefs: schema,
+    resolvers,
+});
+
+server.applyMiddleware({ app, path: '/graphql' });
 
 // api routes
 require('./routes')(router);
